80caa503525268ce2d65706ad1dca0e086c5132c
Sebastian Seijo <sebastiangseijo@gmail.com>
2019-09-15 22:25:59 +0200

    add edit button and navigation

--- src/app/contacts/contacts-list/contacts-list.component.ts
+++ src/app/contacts/contacts-list/contacts-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Contact} from '../models/contact';
 import {StorageService} from '../services/storage.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-contacts-list',
@@ -10,10 +11,14 @@ import {StorageService} from '../services/storage.service';
 export class ContactsListComponent implements OnInit {
   contacts: Contact[] = [];
 
-  constructor(private storageService: StorageService) {
+  constructor(private storageService: StorageService, private router: Router) {
   }
 
   ngOnInit() {
     this.contacts = this.storageService.getContacts();
+  }
+
+  editContact(contact: Contact) {
+    this.router.navigate(['edit-contact', contact.id]);
   }
 }
