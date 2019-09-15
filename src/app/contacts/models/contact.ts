af895dddfa6448e6f5a63667d7c2d77689ee535e
Sebastian Seijo <sebastiangseijo@gmail.com>
2019-09-15 20:24:06 +0200

    contact model modification for reactive forms

--- src/app/contacts/models/contact.ts
+++ src/app/contacts/models/contact.ts
@@ -1,11 +1,11 @@
 import * as uuid from 'uuid';
 
 export class Contact {
-  private readonly id: string;
-  private firstName: string;
-  private lastName: string;
-  private email: string;
-  private country: string;
+   readonly id: string;
+   firstName: string;
+   lastName: string;
+   email: string;
+   country: string;
 
   constructor(firstName: string, lastName: string, email: string, country: string) {
     this.id = uuid.v4();
@@ -15,39 +15,4 @@ export class Contact {
     this.country = country;
   }
 
-  getId(): string {
-    return this.id;
-  }
-
-  getFirstName(): string {
-    return this.firstName;
-  }
-
-  getLastName(): string {
-    return this.lastName;
-  }
-
-  getEmail(): string {
-    return this.email;
-  }
-
-  getCountry(): string {
-    return this.country;
-  }
-
-  setFirstName(firstName: string) {
-    this.firstName = firstName;
-  }
-
-  setLastName(lastName: string) {
-    this.lastName = lastName;
-  }
-
-  setEmail(email: string) {
-    this.email = email;
-  }
-
-  setCountry(country: string) {
-    this.country = country;
-  }
 }
