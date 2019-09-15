fd335e3e6344c51bb03dc6bc56c0011d75a01ef0
Sebastian Seijo <sebastiangseijo@gmail.com>
2019-09-15 16:37:35 +0200

    add encapsulation and module modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts/contacts-list/contacts-list.component';
 import {ContactsViewComponent} from './contacts/contacts-view/contacts-view.component';
+import { ContactsAddComponent } from './contacts/contacts-add/contacts-add.component';
+import { ContactsFormComponent } from './contacts/contacts-form/contacts-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsListComponent,
-    ContactsViewComponent
+    ContactsViewComponent,
+    ContactsAddComponent,
+    ContactsFormComponent
   ],
   imports: [
     BrowserModule,
