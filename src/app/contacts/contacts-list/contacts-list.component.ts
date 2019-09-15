942400eabe5238fb1093db8462c377823c5bd673
Sebastian Seijo <sebastiangseijo@gmail.com>
2019-09-15 20:26:37 +0200

    add form validation, reactive form and storage service to component

--- src/app/contacts/contacts-list/contacts-list.component.ts
+++ src/app/contacts/contacts-list/contacts-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Contact} from '../models/contact';
+import {StorageService} from '../services/storage.service';
 
 @Component({
   selector: 'app-contacts-list',
@@ -9,9 +10,10 @@ import {Contact} from '../models/contact';
 export class ContactsListComponent implements OnInit {
   contacts: Contact[] = [];
 
-  ngOnInit() {
-    this.contacts.push(new Contact('Sebastian', 'Seijo', 'sebastiangseijo@gmail.com', 'ar'));
-    this.contacts.push(new Contact('Micaela', 'Barrio', 'micabarrio@gmail.com', 'ar'));
+  constructor(private storageService: StorageService) {
   }
 
+  ngOnInit() {
+    this.contacts = this.storageService.getContacts();
+  }
 }
