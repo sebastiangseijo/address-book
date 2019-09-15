c03777dd89375d85548f5ff7b3e664dd9e532de9
Sebastian Seijo <sebastiangseijo@gmail.com>
2019-09-15 16:37:01 +0200

    add contacts component

---
+++ src/app/contacts/contacts-add/contacts-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-add',
+  templateUrl: './contacts-add.component.html',
+  styleUrls: ['./contacts-add.component.scss']
+})
+export class ContactsAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
